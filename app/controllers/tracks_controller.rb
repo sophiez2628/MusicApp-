69547522f75e624757ab407285849786098f1261
Sophie Zhao <sophie.zhao@ucla.edu>
2015-09-18 08:04:53 -0700

    added ability for user to edit or delete a track

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -15,10 +15,33 @@ class TracksController < ApplicationController
     end
   end
 
+  def edit
+    @track = Track.find_by(id: params[:id])
+    @albums = Album.all
+    @current_album_id = @track.album_id
+    render :edit
+  end
+
+  def update
+    @track = Track.find_by(id: params[:id])
+    if @track.update(track_params)
+      render :show
+    else
+      render :edit
+    end
+  end
+
   def show
     @track = Track.find_by(id: params[:id])
+    @track_album = Album.find_by(id: @track.album_id).name
     render :show
   end
+
+  def destroy
+    @track = Track.find_by(id: params[:id])
+    @track.destroy
+    render :new
+  end 
 
   private
   def track_params
